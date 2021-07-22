9a1c6466f1a321683feb178d121e34a8a52005eb
uZJl <1969643139@qq.com>
2021-07-22 23:39:45 +0800

    0722

--- 0721/src/sort/BucketSort.java
+++ 0721/src/sort/BucketSort.java
@@ -1,14 +1,55 @@
 package sort;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by JiaLe on 2021/7/21 22:32
  */
 public class BucketSort {
     public static void main(String[] args) {
-
+        int[] arr = new int[]{1,5,89,6,7,1,3};
+        bucketSort(arr);
+        for (int num : arr) {
+            System.out.print(num);
+        }
     }
 
     public static void bucketSort(int[] arr) {
-        // TODO: 2021/7/21  
+        // TODO: 2021/7/21
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max,arr[i]);
+            min = Math.min(min,arr[i]);
+        }
+
+        //计算桶的数量
+        int bucketNum = (max - min) / arr.length + 1;
+        ArrayList<ArrayList<Integer>> bucketArr = new ArrayList<>(bucketNum);
+        for (int i = 0; i < bucketNum; i++) {
+            bucketArr.add(new ArrayList<>());
+        }
+
+        //将每个元素放入桶中
+        for (int i = 0; i < arr.length; i++) {
+            int num = (arr[i] - min) / arr.length;
+            bucketArr.get(num).add(arr[i]);
+        }
+
+        //对每个桶进行排序
+        for (int i = 0; i < bucketArr.size(); i++) {
+            Collections.sort(bucketArr.get(i));
+        }
+
+        //将桶中的元素赋值给原数组
+        int index = 0;
+        for (int i = 0; i < bucketArr.size(); i++) {
+            for (int j = 0; j < bucketArr.get(i).size(); j++) {
+                arr[index++] = bucketArr.get(i).get(j);
+            }
+        }
+
+
     }
 }
--- 0721/src/sort/RadixSort.java
+++ 0721/src/sort/RadixSort.java
@@ -1,13 +1,56 @@
 package sort;
 
+import java.util.Arrays;
+
 /**
  * Created by JiaLe on 2021/7/21 22:46
  */
 public class RadixSort {
     public static void main(String[] args) {
-        
+        int[] arr = new int[]{1,5,89,6,7,1,3};
+        radixSort(arr);
+        for (int num : arr) {
+            System.out.print(num);
+        }
     }
-    public static void radixSort() {
-        // TODO: 2021/7/21  
+    public static void radixSort(int[] arr) {
+        // TODO: 2021/7/21
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max,arr[i]);
+        }
+
+        //最大数的长度
+        int maxLen = (max+"").length();
+
+        //定义一个二维数组，表示10个桶，每个桶就是一个数组
+        //为了在放入数的时候数据溢出，每个桶大小定义为arr.length
+        int[][] bucket = new int[10][arr.length];
+
+        //定义一个一维数组来记录各个桶每次放入数据的个数
+        int[] bucketElementCount = new int[10];
+
+        for (int i = 0, n = 1; i < maxLen; i++, n *= 10) {
+            //对每个元素对应位进行处理
+            for (int j = 0; j < arr.length; j++) {
+                int digiOfElement = arr[j] / n % 10;
+                bucket[digiOfElement][bucketElementCount[digiOfElement]] = arr[j];
+                bucketElementCount[digiOfElement]++;
+            }
+            //按照这个桶的顺序即一维数组的下标一次取出数据放入原来数组
+            int index = 0;
+
+            for (int k = 0; k < bucketElementCount.length; k++) {
+                if (bucketElementCount[k] != 0) {
+                    for (int m = 0; m < bucketElementCount[k]; m++) {
+                        arr[index++] = bucket[k][m];
+                    }
+                }
+                bucketElementCount[k] = 0;
+            }
+
+            System.out.println("第"+(i+1)+"轮，排序处理 arr="+ Arrays.toString(arr));
+        }
+
     }
 }
